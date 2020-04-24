be60d841a1dc4236d92ab225e79aae13f8600d79
Renato Dourado <renatodourado@gmail.com>
2020-04-24 02:19:24 -0300

    Landing Page, criação básico

---
+++ src/components/FinalSurvey.vue
@@ -0,0 +1,72 @@
+﻿<template>
+  <div class="survey secondary bg-white">
+    <HeaderGlobal :secondaryHeader="true" v-if="!popupPrivActive" />
+    <div class="logo-bg" />
+    <!-- Page Content Desktop -->
+    <div class="container-fluid">
+      <div class="container container-content border-left-light pt-5 pb-5">
+        <div class="row">
+          <div class="col-md-6 offset-md-3 text-center pl-md-5 pr-md-0 px-5">
+            <!--<div class="col-lg-6 pl-5 text-center">-->
+            <div class="text-center">
+              <img
+                src="static/assets/img/fim-de-jogo.png"
+                class="img-fluid w-md-25 img-center px-4 px-lg-0 pt-2"
+                alt="Fim de jogo"
+              />
+              <h2 class="text-secondary extra-bold mt-5">Fim de jogo!</h2>
+              <h5 class="text-secondary extra-bold mb-4">Apita o árbitro</h5>
+              <hr />
+              <h3 class="text-black bold pb-1 mt-4">
+                Obrigado,
+                <span class="extra-bold">{{nome}}</span>
+              </h3>
+              
+            </div>
+            <h4 class="text-black mb-4">
+              <strong>Agradecemos muito a sua presença e o seu tempo para responder nossa pesquisa.</strong>
+            </h4>                          
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <FooterGlobal :secondaryFooter="true" />
+  </div>
+</template>
+
+<script>
+import FooterGlobal from "./FooterGlobal";
+import HeaderGlobal from "./HeaderGlobal";
+
+export default {
+  name: "SurveyFinal",
+  data: () => ({
+    qtSomos: 0
+  }),
+  components: {
+    FooterGlobal,
+    HeaderGlobal
+        },
+
+  computed: {
+    userId() {
+      return this.$store.state.userId;
+    },
+      nome() {
+
+      var n = this.$store.state.guestData.nome;
+      return n;
+    },
+    sobreNome() {
+      var n = this.$store.state.guestData.sobreNome;
+      return n;
+    }
+  },
+  created() {
+    this.$gtag.event("FinalSurvey", { method: "Google" });
+  },
+  methods: {
+  }
+};
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,525 @@
+<template>
+  <div class="home">
+    <HeaderGlobal :secondaryHeader="false" />
+    <main>
+      <div class="container px-4 px-lg-0 pt-5 min-vh-100">
+        <div class="row pt-5">
+          <div class="col-lg-9 col-12 pt-5">
+            <h1 class="text-white">
+              <strong>
+                Centavos que
+                valem cesta de 3!
+              </strong>
+            </h1>
+          </div>
+          <div class="col-12 col-lg-9 pt-4">
+            <h4 class="text-yellow">
+              <strong>
+                Sabia que você pode ajudar os jogadores
+                de basquete que mais precisam doando
+                seus centavos?
+              </strong>
+            </h4>
+          </div>
+          <div class="col-12 col-lg-9 py-4">
+            <h5 class="text-white">
+              Ao cadastrar o seu Cartão de Crédito Mastercard* em nossa
+              plataforma, todas as suas compras serão arredondadas
+              automaticamente e a diferença dos centavos será doada para a
+              Associação dos Atletas xxxxxxxxx
+            </h5>
+          </div>
+          <div class="col-lg-3 col-12 py-3 pb-5 py-lg-5">
+            <button class="btn btn-block btn-primary rounded-0">Quero doar</button>
+          </div>
+        </div>
+      </div>
+    </main>
+    <section role="how-it-works" class="bg-white min-vh-100">
+      <div class="container-fluid bg-primary">
+        <div class="row">
+          <div class="col-12 col-lg-6 offset-lg-3 px-lg-5 my-5">
+            <h5 class="text-white text-center px-lg-5 py-2">
+              <strong>
+                Os valores arrecadados pela CBB com a
+                doação dos seus centavos nesta campanha
+                serão integralmente direcionados para as
+                famílias dos jogadores de basquete que mais
+                precisam neste momento de grande
+                dificuldade para todos.
+              </strong>
+            </h5>
+          </div>
+        </div>
+      </div>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-8 offset-2 col-lg-4 offset-lg-4 bg-white pt-lg-5 mt-5">
+            <h4 class="text-black text-center mb-3">
+              <strong>Como funciona</strong>
+            </h4>
+            <div class="title-bar" />
+          </div>
+        </div>
+        <div class="row px-5 py-5">
+          <div class="col-lg-3">
+            <div class="row">
+              <div class="col-3">
+                <h1 class="text-yellow text-right">
+                  <strong>1</strong>
+                </h1>
+              </div>
+              <div class="col-9 pt-5">
+                <h6 class="text-black">
+                  Cadastrando seu
+                  Cartão de Crédito
+                  Mastercard*, você se
+                  torna um doador da
+                  <strong>CBB</strong>.
+                </h6>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3">
+            <div class="row">
+              <div class="col-3">
+                <h1 class="text-yellow text-right">
+                  <strong>2</strong>
+                </h1>
+              </div>
+              <div class="col-9 pt-5">
+                <h6 class="text-black">
+                  Em uma compra de
+                  <strong>R$8,30</strong> o sistema
+                  arredonda os
+                  <strong>R$0,70</strong>
+                  restantes para
+                  completar
+                  <strong>R$9,00</strong>.
+                </h6>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3">
+            <div class="row">
+              <div class="col-3">
+                <h1 class="text-yellow text-right">
+                  <strong>3</strong>
+                </h1>
+              </div>
+              <div class="col-9 pt-5">
+                <h6 class="text-black">
+                  Esses
+                  <strong>R$0,70</strong> são
+                  repassados para a
+                  CBB e o valor gasto é
+                  cobrado como
+                  <strong>R$9,00</strong>.
+                </h6>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3">
+            <div class="row">
+              <div class="col-3">
+                <h1 class="text-yellow text-right">
+                  <strong>4</strong>
+                </h1>
+              </div>
+              <div class="col-9 pt-5">
+                <h6 class="text-black">
+                  No final do mês será
+                  feito um único
+                  lançamento na fatura
+                  do seu cartão de
+                  crédito com o total de
+                  centavos doados.
+                </h6>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section role="how-it-works" class="bg-white min-vh-100 py-5">
+      <div class="container-fluid">
+        <div class="row text-center py-5">
+          <div class="col-lg-3 bg-blue-1 p-5">
+            <h6 class="text-white my-auto">
+              <strong>
+                Os arredondamentos
+                nunca passam de
+                R$0,99 por compra
+              </strong>
+            </h6>
+          </div>
+          <div class="col-lg-3 bg-blue-2 p-5">
+            <h6 class="text-white my-auto">
+              <strong>
+                Você pode
+                descadastrar seu
+                cartão quando quiser,
+                de forma simples e
+                rápida pelo site.
+              </strong>
+            </h6>
+          </div>
+          <div class="col-lg-3 bg-blue-3 p-5">
+            <h6 class="text-white my-auto">
+              <strong>
+                A Plataforma
+                InCENTive atende aos
+                principais protocolos
+                globais de segurança
+                digital.
+              </strong>
+            </h6>
+          </div>
+          <div class="col-lg-3 bg-blue-4 p-5">
+            <h6 class="text-white my-auto">
+              <strong>
+                No momento,
+                aceitamos apenas
+                Cartões de Crédito
+                Mastercard. Outros
+                cartões e bandeiras
+                serão aceitos em
+                breve.
+              </strong>
+            </h6>
+          </div>
+          <div class="col-lg-3 mx-lg-auto col-12 px-lg-5 py-3 pb-5 py-lg-5 mt-lg-5">
+            <button class="btn btn-block px-5 btn-primary rounded-0 my-3 my-lg-5">Quero doar</button>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section role="how-it-works" class="bg-white min-vh-100">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-8 offset-2 col-lg-6 offset-lg-3 pt-lg-5 mt-5">
+            <h4 class="text-black text-center mb-3">
+              <strong>Quem usa a Plataforma InCENTive</strong>
+            </h4>
+            <div class="title-bar" />
+          </div>
+        </div>
+        <div class="row text-center py-lg-5 mt-5">
+          <div class="col-4 col-lg-2 pt-5 pt-lg-0">
+            <img src="../../public/static/assets/img/partners/logo-aacd.png" alt="..." />
+          </div>
+          <div class="col-4 col-lg-2 pt-5 pt-lg-0">
+            <img src="../../public/static/assets/img/partners/logo-aacd.png" alt="..." />
+          </div>
+          <div class="col-4 col-lg-2 pt-5 pt-lg-0">
+            <img src="../../public/static/assets/img/partners/logo-aacd.png" alt="..." />
+          </div>
+          <div class="col-4 col-lg-2 pt-5 pt-lg-0">
+            <img src="../../public/static/assets/img/partners/logo-aacd.png" alt="..." />
+          </div>
+          <div class="col-4 col-lg-2 pt-5 pt-lg-0">
+            <img src="../../public/static/assets/img/partners/logo-aacd.png" alt="..." />
+          </div>
+          <div class="col-4 col-lg-2 pt-5 pt-lg-0">
+            <img src="../../public/static/assets/img/partners/logo-aacd.png" alt="..." />
+          </div>
+        </div>
+        <div class="row px-5 py-5">
+          <div class="col-12 col-lg-6 offset-lg-3">
+            <h6 class="text-black text-center">
+              <i>
+                "O brasileiro é solidário e disposto a contribuir, mas a falta de tempo e de canais que possibilitam doações são os principais fatores que não potencializam esse engajamento. Essa nova tecnologia resolve esses dois problemas, pois basta um simples cadastro para realizar doações com pequenos valores, sem mudar nada na rotina.
+                <br />A nossa expectativa é que essa nossa parceria cause uma grande impacto positivo em nosso trabalho, contribuindo de forma significativa para os atendimentos que realizamos diariamente na AACD."
+                <br />
+                <strong>
+                  Edson Brito, Superintendente de Marketing
+                  <br />e Relações Internacionais da AACD
+                </strong>
+              </i>
+            </h6>
+          </div>
+        </div>
+      </div>
+    </section>
+    <footer role="how-it-works" class="bg-secondary min-vh-100">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12 pt-lg-5 text-center mt-5">
+            <a class="navbar-brand" href="index.html">
+              <img
+                src="static/assets/img/logo.png"
+                class="px-3 px-lg-0 py-2"
+                alt="Cadastra Portuguesa Santista"
+                height="136"
+              />
+            </a>
+          </div>
+          <div class="col-12 pt-lg-5 text-center mt-5">
+            <ul class="list-group">
+              <li class="list-group-item bg-transparent border-0">
+                <h6>
+                  <a href="#" class="text-white">Sobre</a>
+                </h6>
+              </li>
+              <li class="list-group-item bg-transparent border-0">
+                <h6>
+                  <a href="#" class="text-white">Termos de Uso</a>
+                </h6>
+              </li>
+              <li class="list-group-item bg-transparent border-0">
+                <h6>
+                  <a href="#" class="text-white">Política de Privacidade</a>
+                </h6>
+              </li>
+            </ul>
+          </div>
+          <div class="col-12 pt-lg-5 text-center mt-5">
+            <h6 class="text-uppercase text-white">Powered by</h6>
+          </div>
+          <div class="col-12 pt-lg-4 text-center">
+            <img
+              src="static/assets/img/partners/logo-incentive.png"
+              class="px-3 px-lg-0 py-2"
+              alt="Cadastra Portuguesa Santista"
+            />
+          </div>
+          <div class="col-12 pt-lg-4 text-center">
+            <img
+              src="static/assets/img/partners/logo-fanbase.png"
+              class="px-3 px-lg-0 py-2"
+              alt="Cadastra Portuguesa Santista"
+            />
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+import HeaderGlobal from "./HeaderGlobal.vue";
+import NumberText from "./widgets/NumberText.vue";
+// import FooterGlobal from "./FooterGlobal.vue";
+
+// import { Survey } from "../services/Survey.js";
+// const s = new Survey();
+
+// import { Guest } from "../em1click/SmartSurvey.js";
+// const g = new Guest();
+
+//     import { mask } from "vue-the-mask";
+export default {
+  name: "Home",
+  components: {
+    HeaderGlobal,
+    NumberText
+    // FooterGlobal
+  }
+  //   data: () => ({
+  //     btnMes: "Quero responder",
+  //     nome: "",
+  //     sobreNome: "",
+  //     rg: "",
+  //     cpf: "",
+  //     phone: "",
+  //     email: "",
+  //     errors: [],
+  //       qtErros: 0,
+  //     surv: {}
+  //   }),
+  //   directives: { mask },
+  //   created() {},
+  //   mounted() {},
+  //   watch: {},
+  //  computed: {
+  //     inputInvalid() {
+  //       if (this.qtErros == 0) return false;
+  //       else return true;
+  //     }
+  //   },
+  //  methods: {
+  //     submit() {
+  //       //this.$router.push("home");
+  //     },
+
+  //     checkForm() {
+  //       this.errors = [];
+  //         this.qtErros = 0;
+
+  //       if (!this.nome) {
+  //         this.ErrorOccured("Informe seu Nome.", 1);
+  //       }
+
+  //       if (!this.sobreNome) {
+  //         this.ErrorOccured("Informe seu sobreNome.", 2);
+  //       }
+
+  //       if (!this.rg) {
+  //         this.ErrorOccured("Informe seu RG.", 3);
+  //       }
+
+  //       if (!this.cpf) {
+  //         this.ErrorOccured("Informe seu CPF.", 4);
+  //       } else {
+  //         if (!this.TestaCPF(this.cpf))
+  //           this.ErrorOccured("Verifique, CPF Inválido", 4);
+  //       }
+
+  //       if (!this.phone) {
+  //         this.ErrorOccured("Informe seu número de celular.", 5);
+  //       } else {
+  //         if (!this.TestaPhone(this.phone)) {
+  //           this.ErrorOccured("Verifique, número de celular inválido.", 5);
+  //         }
+  //       }
+
+  //       if (!this.email) {
+  //         this.ErrorOccured("Informe seu E-mail.", 6);
+  //       } else {
+  //         if (!this.TestaEmail(this.email))
+  //           this.ErrorOccured("Verifique, E-mail Inválido", 6);
+  //       }
+
+  //         var ok = false;
+
+  //         if (this.errors)
+  //         {
+  //             if (this.errors.length == 0)
+  //                 ok = true;
+  //         }
+  //         else
+  //         {
+  //         ok = true;
+  //         }
+
+  //       if (ok) {
+  //         var payload = {
+  //             cpf: this.cpf,
+  //             rg: this.rg,
+  //             nome: this.nome,
+  //             sobreNome: this.sobreNome,
+  //             celular: this.phone,
+  //             email: this.email
+  //           };
+
+  //           this.$store.commit("SET_GuestData", payload);
+  //            g.UpdateGuest(payload).then(ret => {
+  //                if (ret) { //atualizamos os dados do convidado e chamamos o survey
+  //                          s.Start(this.cpf, this.phone).then(
+  //                             ret => {
+  //                               this.$store.commit("SET_SurveyStruct", ret.surveyStruct);
+  //                               this.$store.commit("SET_Req", 0);
+  //                               this.$store.commit("SET_Chapter", 0);
+  //                               this.$store.commit("SET_SurveyUserId", ret.userSurveyId);
+  //                               this.$store.commit("SET_UserId", this.cpf);
+  //                               this.$store.commit("SET_LastReq", 0);
+  //                               this.$store.commit("SET_LastChapter", 0);
+  //                               this.surv = ret;
+  //                               this.$router.push("survey");
+  //                             },
+  //                             err => alert(err)
+  //                           );
+  //                         }
+  //                     },
+  //                 err => this.ErrorOccured(err.msg, 10)
+  //             );
+  //       }
+  //       else window.scrollTo(0, 0);
+  //       //e.preventDefault();
+  //     },
+  //     ErrorOccured(value, id) {
+  //       var payload = { questid: id, msg: value };
+
+  //       if (this.errors) {
+  //         var found = false;
+  //         for (var i = 0; i < this.errors.length; i++) {
+  //           if (this.errors[i].questid == id) {
+  //             this.errors[i] = payload;
+  //             found = true;
+  //           }
+  //         }
+  //         if (!found) {
+  //           this.errors.push(payload);
+  //           this.qtErros++;
+  //         }
+  //       } else {
+  //         this.qtErros++;
+  //         this.errors.push(payload);
+  //       }
+  //     },
+  //     TestaCPF: function(strCPF) {
+  //       var Soma;
+  //       var Resto;
+  //       Soma = 0;
+  //       strCPF = strCPF.replace(/[^\d]/g, "");
+  //       if (strCPF == "00000000000") return false;
+  //       if (strCPF == "11111111111") return false;
+  //       if (strCPF == "22222222222") return false;
+  //       if (strCPF == "33333333333") return false;
+  //       if (strCPF == "44444444444") return false;
+  //       if (strCPF == "55555555555") return false;
+  //       if (strCPF == "66666666666") return false;
+  //       if (strCPF == "77777777777") return false;
+  //       if (strCPF == "88888888888") return false;
+  //       if (strCPF == "99999999999") return false;
+  //       var i;
+  //       for (i = 1; i <= 9; i++)
+  //         Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+  //       Resto = (Soma * 10) % 11;
+
+  //       if (Resto == 10 || Resto == 11) Resto = 0;
+  //       if (Resto != parseInt(strCPF.substring(9, 10))) return false;
+
+  //       Soma = 0;
+  //       for (i = 1; i <= 10; i++)
+  //         Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+  //       Resto = (Soma * 10) % 11;
+
+  //       if (Resto == 10 || Resto == 11) Resto = 0;
+  //       if (Resto != parseInt(strCPF.substring(10, 11))) return false;
+  //       return true;
+  //     },
+  //     TestaRG: function(rg) {
+  //       // value = document.getElementById("cpf").value;
+  //       let strRG = rg.replace(/[^\d]/g, "");
+  //       if (strRG == "000000000") return false;
+  //       if (strRG == "111111111") return false;
+  //       if (strRG == "222222222") return false;
+  //       if (strRG == "333333333") return false;
+  //       if (strRG == "444444444") return false;
+  //       if (strRG == "555555555") return false;
+  //       if (strRG == "666666666") return false;
+  //       if (strRG == "777777777") return false;
+  //       if (strRG == "888888888") return false;
+  //       if (strRG == "999999999") return false;
+
+  //       var re = /^[0-9]{2}.[0-9]{3}.[0-9]{3}-[0-9]{1}$/;
+  //       if (re.test(rg)) {
+  //         return true;
+  //       } else {
+  //         // form.cpf.focus();
+  //         return false;
+  //       }
+  //     },
+  //     TestaEmail: function(rg) {
+  //       var rgRegex = /\S+@\S+\.\S+/;
+  //       if (rgRegex.test(rg)) {
+  //         return true;
+  //       } else {
+  //         // form.cpf.focus();
+  //         return false;
+  //       }
+  //     },
+
+  //     TestaPhone: function(cel) {
+  //       var celRegex = /(0?[1-9]{2})*\D*(9)\D?(\d{4})+\D?(\d{4})\b/;
+  //       if (celRegex.test(cel)) {
+  //         return true;
+  //       } else {
+  //         // form.cpf.focus();
+  //         return false;
+  //       }
+  //     }
+  //   }
+};
+</script>
